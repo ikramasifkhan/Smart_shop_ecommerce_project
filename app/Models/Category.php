815d2fb1ad2b9a20c9492892cee4f2f6ae6d53e5
ikramasifkhan <iakriubat2016@gmail.com>
2020-03-12 01:07:42 +0600

    boot fucntion added to generating category slug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Category extends Model
 {
     protected $guarded = [];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($category){
+            $category->slug= Str::slug($category->name, '-');
+        });
+    }
 
     public function parent_category(){
         return $this->belongsTo(__CLASS__);
